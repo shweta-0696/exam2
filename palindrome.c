8b66064222544b0cbdc342c53250bd0200d729d2
shweta-0696 <shwetagote0696@gmail.com>
2019-12-06 06:35:42 -0800

    Added Plaindrome file

--- palindrome.c
+++ palindrome.c
@@ -1,29 +1,8 @@
 #include<stdio.h>
 int main()
 {
-   /*int num[3]=123;
-   int rev[5];
-   int i=0;
-   while(num != 0)
-   {
-     int r=(num%10);
-     rev[i++]=r;
-     num=(num/10);
-   }
-   int flag=0;
-   for(int i=0;i<3;i++)
-   {
-      if(num[i]!=rev[i)
-     {
-       flag=1;
-       break;    
-     }
-   }
-   if(flaf == 1)
-      printf("Not palindrome number");
-   else
-      printf("palindrome number");*/
-   printf("hello");
-   
-   return 0;
+
+printf("hello");
+
+return 0;
 }
